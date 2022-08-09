c657407f5fe6bda51cda01d5fa9ed853a7c9a3ef
George Chaves <george_hfc@yahoo.com.br>
2022-08-10 00:42:39 +0100

    seeds working

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,12 +12,20 @@ puts "user 'Severino' created"
 puts 'Creating movies...'
 @movies = [{ title: 'Bacurau',
              year: 2019,
-             duration: 191,
+             duration: 131,
              location: 'Parelhas, Rio Grande do Norte',
              synopsis: "Bacurau, a small settlement in Brazil's remote backcountry, is shaken by the death of its elderly matriarch. But something strange is happening in the village, and there's little time for mourning. The water supply has been cut off, animals are stampeding through the streets, and empty coffins are turning up on the roadside. One morning, the villagers wake up to find their home has disappeared from satellite maps completely. Under threat from an unknown enemy, Bacurau braces itself for a bloody, brutal fight for survival.",
              genre: ["Adventure", "Horror", "Movie"],
              image_url: '../app/assets/images/seeds/bacurau-poster.jpeg',
-             user: severino }]
+             user: severino },
+             { title: "A Dog's Will",
+               year: 2000,
+               duration: 104,
+               location: 'Cabaceiras, Paraíba',
+               synopsis: "João Grilo and Chicó are two very poor and clever Brazilian Northeasterners who struggle for survival and trick people to get by. After meeting the wealthy Rosinha, both hope to finally strive in life, but their plans are interrupted by the arrival of an outlaw.",
+               genre: ["Comedy", "Drama", "Fantasy"],
+               image_url: '../app/assets/images/seeds/a-dogs-will-poster.jpg',
+               user: severino }]
 @movies.each do |attributes|
   movie = Movie.create!(attributes)
   puts "Created #{movie.title}"
