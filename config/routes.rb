988f4940c64d9ab8e6025b6db28941c6ec6000a3
George Chaves <george_hfc@yahoo.com.br>
2022-08-17 00:30:16 +0100

    navbar logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  # devise_for :users
+
   root to: 'pages#home'
   resources :movies do
     resources :watchlists, only: [ :index, :show, :new, :create ]
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
     end
   end
   resources :watchlist, only: [ :destroy ]
-  get "my-watchlist", to: "watchlists#my_watchlist"
-  get "my-movies", to: "movies#my_movies"
+
+  get 'my-watchlist', to: 'watchlists#my_watchlist'
+  get 'my-movies', to: 'movies#my_movies'
 end
