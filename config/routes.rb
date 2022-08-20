c4b3af93cf36a88f0e60aae9369f86628f788ed2
George Chaves <george_hfc@yahoo.com.br>
2022-08-20 16:57:13 +0100

    routes uplift

--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,10 @@ Rails.application.routes.draw do
       get :highlights, :top
     end
   end
-  resources :watchlists, only: [ :show, :create ]
+  resources :watchlists, only: [ :show, :create, :update ]
+  resources :movie_watchlists, only: [ :show, :create, :destroy ] do
+    get :review
+  end
 
-  get 'my-watchlist', to: 'watchlists#my_watchlist'
   get 'my-movies', to: 'movies#my_movies'
 end
